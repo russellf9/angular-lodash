dd9bc0b85980990d7c3183696dc8af6dc95c7f57
Russell Wenban <russell@factornine.co.uk>
2015-01-03 19:21:54 +0000

    Initial commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,57 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('app.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$scope', '_', function ($scope, _) {
+        // examples from Ben Nadel
+        // dependency-injection framework.
 
-}]);
+        // Set up friends collection for demo.
+        var friends = [
+            {
+                id: 1,
+                name: "Sarah",
+                isGoodFriend: true,
+                isBestFriend: false
+            },
+            {
+                id: 2,
+                name: "Tricia",
+                isGoodFriend: false,
+                isBestFriend: false
+            },
+            {
+                id: 3,
+                name: "Kim",
+                isGoodFriend: true,
+                isBestFriend: true
+            },
+            {
+                id: 4,
+                name: "Joanna",
+                isGoodFriend: true,
+                isBestFriend: false
+            }
+        ];
+
+        // Find the good friends.
+        var goodFriends = _.where( friends, "isGoodFriend" );
+
+        // Of the good friends, find the best friends!
+        var bestFriends = _.where( goodFriends, "isBestFriend" );
+
+        // Log us some interesting stats on friendship.
+        console.info( "Good friends: %s", _.naturalList( _.pluck( goodFriends, "name" )));
+        $scope.goodFriends = _.naturalList( _.pluck( goodFriends, "name" ));
+
+
+        console.info( "Best friends: %s", _.naturalList( _.pluck( bestFriends, "name" )));
+        $scope.bestFriends = _.naturalList( _.pluck( bestFriends, "name" ));
+    }]);
--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('myApp.view2 module', function() {
+describe('app.view2 module', function() {
 
   beforeEach(module('myApp.view2'));
 
